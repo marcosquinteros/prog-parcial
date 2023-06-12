64d0c27d02e414e3a1721fdc93703ee3881a9cfc
Santiago Lacki <santiagolacki@gmail.com>
2023-06-12 02:44:42 -0300

    se agrego register y modif del css content

--- urcan-grande-lacki-quinteros/src/Pages/Register.jsx
+++ urcan-grande-lacki-quinteros/src/Pages/Register.jsx
@@ -1,12 +1,90 @@
 import React from "react";
+import { useState } from "react";
 
 const Register = () => {
+  const [nombre,setNombre] = useState("");
+  const [apellido,setApellido] = useState("");
+  const [email,setEmail] = useState("");
+  const [contraseña,setContraseña] = useState("");
+
+  const cargarNombre = (e) => {
+    const value= e.target.value;
+    console.log(value);
+    setNombre(value);
+  };
+
+  const cargarApellido = (e) => {
+    const value= e.target.value;
+    console.log(value);
+    setApellido(value);
+  };
+
+  const cargarEmail = (e) => {
+    const value= e.target.value;
+    console.log(value);
+    setEmail(value);
+  };
+
+  const cargarContraseña = (e) => {
+    const value= e.target.value;
+    console.log(value);
+    setContraseña(value);
+  };
+
+  const guardarCLick = () => {
+    console.log("Nombre:",nombre, "Apellido:",apellido, "E-mail:",email, "Contraseña:",contraseña );
+  };
+
   return (
     <div>
       <div>
+      <h1>FORMULARIO</h1>
         <div className="w-100">
           <div className="content-sidebar-wrapper">
-            <main className="content">Aqui va el contenido del registro</main>
+            <main className="content">
+              <label>Nombre: </label>
+              <input
+                  type="text"
+                  id="nombre"
+                  name="nombre"
+                  placeholder="Nombre"
+                  autoComplete="off"
+                  value={nombre}
+                  onChange={cargarNombre}
+              ></input>
+              <label>Apellido: </label>
+              <input 
+                  type="text"
+                  id="apellido"
+                  name="apellido"
+                  placeholder="Apellido"
+                  autoComplete="off"
+                  value={apellido}
+                  onChange={cargarApellido}
+              ></input>
+              <label>E-mail: </label>
+              <input 
+                  type="text"
+                  id="email"
+                  name="email"
+                  placeholder="E-mail"
+                  autoComplete="off"
+                  value={email}
+                  onChange={cargarEmail}
+              ></input>
+              <label>Contraseña: </label>
+              <input 
+                  type="password"
+                  id="contraseña"
+                  name="contraseña"
+                  placeholder="Contraseña"
+                  autoComplete="off"
+                  value={contraseña}
+                  onChange={cargarContraseña}
+              ></input>
+              
+              <button onClick={guardarCLick}>Guardar</button>
+            </main>
           </div>
         </div>
       </div>
