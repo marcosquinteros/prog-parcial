1c3fd6c7035625888f5dc6c98f0fc718ea0aaa5d
mwarcos <mmwarcos@gmail.com>
2023-06-19 09:44:51 -0300

    cambios en nosotros

--- urcan-grande-lacki-quinteros/src/Pages/Nosotros.jsx
+++ urcan-grande-lacki-quinteros/src/Pages/Nosotros.jsx
@@ -5,7 +5,7 @@ import Comentarios from "../Components/Comentarios";
 const Nosotros = () => {
   return (
     <div className="w-100">
-      <main className=" container">
+      <main className="nosotros-container">
         <h1 className="text-center pt-5 nosotros-text title">
           ¿Quienes somos?
         </h1>
